81f66e09c596e651136dab37ab8f6a595e924ad9
Raphaël Grand <raphael.grand@zenika.com>
2023-01-10 19:06:03 +0100

    Fix deploy file

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:lne0nl/quiz-front.git main:gh-pages
+git push -f git@github.com:lne0nl/quiz-front.git master:gh-pages
 
 cd -
