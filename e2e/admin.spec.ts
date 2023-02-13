939027ed6801f4ba5a71eba91463ae411ae34eda
Raphaël Grand <raphael.grand@zenika.com>
2023-02-13 11:35:54 +0100

    Test create and delete quiz

--- e2e/admin.spec.ts
+++ e2e/admin.spec.ts
@@ -1,28 +1,28 @@
-import { test, expect, firefox } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.describe("Test admin part", () => {
-  // let quizID = "";
+  let quizID = "";
 
   test("Create a quiz", async ({ page }) => {
     await page.goto("/#/admin");
-    await page.screenshot({ path: "screenshots/screenshot.png" });
     const input = page.locator("input.quiz-name-input");
     await expect(input).toBeVisible();
-    // await input.type("Quiz");
-    // await page.keyboard.press("Enter");
-    // await expect(input).not.toBeVisible();
-    // quizID = page.url().substring(page.url().lastIndexOf("/") + 1);
+    await input.type("Quiz");
+    await page.keyboard.press("Enter");
+    await expect(input).not.toBeVisible();
+    quizID = page.url().substring(page.url().lastIndexOf("/") + 1);
   });
 
-  // test("Start a quiz", async ({ page }) => {
-  //   await page.goto(`/#/admin/${quizID}`);
-  //   await page.getByText("Start quiz").click();
-  //   await expect(page.locator("div.quiz-name")).toHaveText("Quiz");
-  // });
+  test("Start a quiz", async ({ page }) => {
+    await page.goto(`/#/admin/${quizID}`);
+    await page.screenshot({ path: "screenshots/screenshot.png" });
+    await page.getByText("Start quiz").click();
+    await expect(page.locator("div.quiz-name")).toHaveText("Quiz");
+  });
 
-  // test("Delete a quiz", async ({ page }) => {
-  //   await page.goto(`/#/admin/${quizID}`);
-  //   await page.getByText("Delete quiz").click();
-  //   await expect(page.locator("input.quiz-name-input")).toBeVisible();
-  // });
+  test("Delete a quiz", async ({ page }) => {
+    await page.goto(`/#/admin/${quizID}`);
+    await page.getByText("Delete quiz").click();
+    await expect(page.locator("input.quiz-name-input")).toBeVisible();
+  });
 });
